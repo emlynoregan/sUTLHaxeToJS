9827c6846588f53b398807220d5ae60485720a93
Emlyn O'Regan <emlynoregan@gmail.com>
2018-01-08 16:25:28 +1030

    Changed sUTLHaxeJS to not be a submodule

--- scripts/pushbuild.sh
+++
@@ -1,8 +0,0 @@
-echo Pushing latest build
-
-cd "./sUTLHaxeJS" || { echo '060' ; exit 1; }
-git add . || { echo '070' ; exit 1; }
-if git commit -a -m "auto"
-then git push origin master || { echo '090' ; exit 1; }
-else echo skip push JS
-fi
